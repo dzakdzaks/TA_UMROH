5bb58d89dfd5ea08b03f0c02e5e264643d19c585
dzaky <dzakyrahmanto@gmail.com>
2019-07-24 21:30:42 +0700

    fix bug

--- app/src/main/java/com/dzakdzaks/ta_umroh/global/GlobalVariable.java
+++ app/src/main/java/com/dzakdzaks/ta_umroh/global/GlobalVariable.java
@@ -6,6 +6,6 @@ import android.widget.ProgressBar;
 
 public class GlobalVariable {
 //    public static final String BASE_URL = "http://192.168.0.21/ta_umroh/";
-    public static final String BASE_URL = "http://192.168.0.17/ta_umroh/";
+    public static final String BASE_URL = "http://192.168.0.10/ta_umroh/";
 //    public static final String BASE_URL = "http://dzakdzaks.000webhostapp.com/ta_umroh/";
 }
--- app/src/main/java/com/dzakdzaks/ta_umroh/retrofit/InitLibrary.java
+++ app/src/main/java/com/dzakdzaks/ta_umroh/retrofit/InitLibrary.java
@@ -3,15 +3,29 @@ package com.dzakdzaks.ta_umroh.retrofit;
 
 import com.dzakdzaks.ta_umroh.global.GlobalVariable;
 
+import okhttp3.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InitLibrary {
 
+    private static Retrofit retrofit = null;
+
+
 
         public static Retrofit setInit() {
-            return new Retrofit.Builder().baseUrl(GlobalVariable.BASE_URL)
+
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+            return new Retrofit.Builder()
+                    .baseUrl(GlobalVariable.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
 
